3d14fbba8a6768ce43a798da73ad001446042298
Alain Felger <alain.felger@googlemail.com>
2025-07-08 16:05:34 +0200

    added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const proxyConf = {
   target: 'http://localhost:5000',
@@ -8,5 +9,35 @@ const proxyConf = {
 
 export default defineConfig({
   root: '.', // your project root
-  base: '/SinglePageProjectEditor/', // base path for the project
+  base: '/SinglePageProjectEditor/', // base path for the project,
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'SPE - Single Page Editor',
+        short_name: 'SPE',
+        description: 'A single page editor for quick edits and project management',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 });
